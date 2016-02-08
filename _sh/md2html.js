d39184deb0df03eb66d94632f6aa40d50996d51a
leader22 <y.sugiura.0316@gmail.com>
2016-02-08 18:38:31 +0900

    watch

--- _sh/md2html.js
+++ _sh/md2html.js
@@ -1,15 +1,20 @@
 const exec = require('child_process').exec;
-const target = process.argv[2];
+const target  = process.argv[2];
+const isWatch = process.argv[3] ? 'watch' : '';
 
 if (target === undefined) {
   console.log('  Usage:');
   console.log('    npm run md2html <targetDir>');
+  console.log('    npm run md2html <targetDir: String> [,<isWatch: Boolean>]');
   console.log('');
 
   process.exit(0);
 }
 
-exec(`npm run cleaver ./${target}/index.md -- --output ./${target}/index.html`, (err, stdout, stderr) => {
+if (isWatch) {
+  console.log('watching changes...');
+}
+exec(`npm run cleaver ${isWatch} ./${target}/index.md -- --output ./${target}/index.html`, (err, stdout, stderr) => {
   if (err || stderr) {
     console.error(err || stderr);
     process.exit(1);
