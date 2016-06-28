d48fa37990552c8317b9c37e17ad4d4e8bfe5ab5
leader22 <y.sugiura.0316@gmail.com>
2016-06-28 22:55:49 +0900

    use es5

--- public/mobile-controls.js
+++ public/mobile-controls.js
@@ -1,11 +1,11 @@
 // モバイルでcontrolsない場合に詰むので
 (function(global) {
-  const isMobile = 'ontouchstart' in global;
+  var isMobile = 'ontouchstart' in global;
   if (isMobile === false) return;
 
   document.body.addEventListener('click', (ev) => {
-    const isRight = (global.innerWidth / 2) < ev.clientX;
-    const dir = isRight ? 1 : -1;
+    var isRight = (global.innerWidth / 2) < ev.clientX;
+    var dir = isRight ? 1 : -1;
     global.navigate(dir);
   }, false);
 }(this));
--- public/timer.js
+++ public/timer.js
@@ -1,12 +1,12 @@
 // 時間管理するの面倒なので経過時間をこっそり出す
 (function(global) {
-  const timer = document.createElement('div');
+  var timer = document.createElement('div');
   timer.textContent = '0';
   timer.id = 'timer';
   document.body.appendChild(timer);
 
   setInterval(function() {
-    const now = timer.textContent|0;
+    var now = timer.textContent|0;
     timer.textContent = '' + (now + 1);
   }, 1000 * 1);
 }(this));
